8331e345ac49e80267ecc71376c07748fb1e5795
Christian Trautman <ctrautma@redhat.com>
2016-02-11 18:13:38 -0500

    Fixing Pycharm install

--- DevVSPerf.sh
+++ DevVSPerf.sh
@@ -18,7 +18,9 @@ cp passwd ~/.vnc/passwd
 # setup Pycharm
 cd ~
 wget https://download.jetbrains.com/python/pycharm-community-5.0.4.tar.gz
-tar xvzf pycharm-community*.tar.gz -C /opt/pycharm-community
+tar xvzf pycharm-community*.tar.gz -C /tmp/
+chown -R root:root /tmp/pycharm*
+mv /tmp/pycharm-community* /opt/pycharm-community
 ln -s /opt/pycharm-community/bin/pycharm.sh /usr/local/bin/pycharm
 ln -s /opt/pycharm-community/bin/inspect.sh /usr/local/bin/inspect
 
